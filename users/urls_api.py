634b5902825a29bee8fc10ca78c578110d057c7a
tinnguyentg <tinnguyen121221@gmail.com>
2022-05-16 17:02:54 +0700

    add JWT authentication

--- users/urls_api.py
+++ users/urls_api.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import views
 
@@ -9,4 +13,6 @@ urlpatterns = [
     path(
         "password_change/", views.PasswordChangeView.as_view(), name="password_change"
     ),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
